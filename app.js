c2bf0e17010db8612dbf3e9a409b857db123dd93
a41915one <a41915one@gmail.com>
2023-07-02 14:28:12 +0800

    feat: add index.hbs content

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-
+const SERVER = 'http://localhost:3000/'
 const mongoose = require('mongoose')
 const URL = require('./models/url')
-const shortenURL = require('./utils/shortenURL')
+const shortenCode = require('./utils/shortenCode')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 if (process.env.NODE_ENV !== 'production') {
@@ -32,23 +32,34 @@ app.get('/', (req, res) => {
 
 // 新增網址
 app.post('/', (req, res) => {
-  // 設定OriginalURL變數，並將網頁中輸入的original賦值給變數
-  const OriginalURL = res.body.original
-  // 去URL models找尋，如果沒有找到跟網頁中輸入的original一樣資料
-  URL.findOne({ OriginalURL })
-    // 就執行這個then，創造一筆original資料並同時執行shortenURL功能創造一筆shorten資料
+  if (!req.body.url) return res.redirect('/')
+  // 先把產生一筆短code的功能賦值到變數中，後續方便使用
+  const shortURL = shortenCode();
+  // 去URL models找尋，如果沒有找到跟網頁中輸入的url一樣資料
+  URL.findOne({ original: req.body.url })
+    // 就執行這個then，創造一筆original資料並同時放入shortURL(以shortenCode所做出的一筆shorten資料)
     .then(data => {
-      data ? data : URL.create({ shorten: shortenURL, original: OriginalURL })
+      // 確保沒有一樣的data才繼續創造
+      if (!data) {
+        return URL.create({ shorten: shortURL, original: req.body.url });
+      }
+      console.log(data);
+      //如果有return data,則會在整個.then()鏈繼續使用，若沒有，怎僅限於在這個.then()中存在
+      return data;
     })
     //如果在URL model中有找到一樣的資料，則不創造新資料，直接渲染原網頁
     .then(data => {
       res.render('index', {
-        original: data.original,
-        shorten: data.shorten,
+        SERVER: SERVER,
+        origin: req.body.url,
+        shortURL: data.shorten,
       })
     })
-    .catch(err => console.log('error !!!'))
+    .catch(err => console.error('error !!!', err));
 })
+
+
+
 
 app.listen(3000, () => {
   console.log('App is listening on http://localhost:3000')
--- utils/shortenCode.js
+++ utils/shortenCode.js
@@ -1,12 +1,14 @@
 function generateShortCode() {
   const Base_62_Char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  const shortCode = ""
+  let shortCode = ""
   let expectedLength = "5"
   for (let i = 0; i < expectedLength; i++) {
     const randomIndex = Math.floor(Math.random() * Base_62_Char.length)
     const chosenChar = Base_62_Char[randomIndex]
     shortCode += chosenChar
   }
+  console.log(shortCode)
+  return shortCode;
 }
 
 module.exports = generateShortCode
